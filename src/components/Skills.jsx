bca86bd2e64415ec2963c21b15f9165043c3034c
Andre Barros <andrebarros7201@gmail.com>
2024-08-17 18:58:49 +0100

    add drop-shadow to skills images

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -26,28 +26,44 @@ export default function Skills() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={htmlLogo} alt="HTML logo" />
+            <img
+              src={htmlLogo}
+              alt="HTML logo"
+              className={styles["skills-logo-html"]}
+            />
           </a>
           <a
             href="https://developer.mozilla.org/en-US/docs/Web/CSS"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={cssLogo} alt="CSS logo" />
+            <img
+              src={cssLogo}
+              alt="CSS logo"
+              className={styles["skills-logo-css"]}
+            />
           </a>
           <a
             href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={javascriptLogo} alt="JavaScript logo" />
+            <img
+              src={javascriptLogo}
+              alt="JavaScript logo"
+              className={styles["skills-logo-javascript"]}
+            />
           </a>
           <a
             href="https://react.dev/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={reactLogo} alt="React logo" />
+            <img
+              src={reactLogo}
+              alt="React logo"
+              className={styles["skills-logo-react"]}
+            />
           </a>
         </div>
       </div>
@@ -60,35 +76,55 @@ export default function Skills() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={nodeLogo} alt="node logo" />
+            <img
+              src={nodeLogo}
+              alt="Node.js logo"
+              className={styles["skills-logo-node"]}
+            />
           </a>
           <a
             href="https://expressjs.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={expressLogo} alt="express logo" />
+            <img
+              src={expressLogo}
+              alt="Express.js logo"
+              className={styles["skills-logo-express"]}
+            />
           </a>
           <a
             href="https://www.postgresql.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={postgresqlLogo} alt="postgresql logo" />
+            <img
+              src={postgresqlLogo}
+              alt="PostgreSQL logo"
+              className={styles["skills-logo-postgresql"]}
+            />
           </a>
           <a
             href="https://www.prisma.io/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={prismaLogo} alt="prisma orm logo" />
+            <img
+              src={prismaLogo}
+              alt="Prisma ORM logo"
+              className={styles["skills-logo-prisma"]}
+            />
           </a>
           <a
             href="https://restfulapi.net/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={apiLogo} alt="API logo" />
+            <img
+              src={apiLogo}
+              alt="API logo"
+              className={styles["skills-logo-api"]}
+            />
           </a>
         </div>
       </div>
@@ -101,35 +137,55 @@ export default function Skills() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={gitLogo} alt="git logo" />
+            <img
+              src={gitLogo}
+              alt="Git logo"
+              className={styles["skills-logo-git"]}
+            />
           </a>
           <a
             href="https://jestjs.io/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={jestLogo} alt="jest logo" />
+            <img
+              src={jestLogo}
+              alt="Jest logo"
+              className={styles["skills-logo-jest"]}
+            />
           </a>
           <a
             href="https://vitejs.dev/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={viteLogo} alt="vite logo" />
+            <img
+              src={viteLogo}
+              alt="Vite logo"
+              className={styles["skills-logo-vite"]}
+            />
           </a>
           <a
             href="https://code.visualstudio.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={vscodeLogo} alt="visual studio code logo" />
+            <img
+              src={vscodeLogo}
+              alt="Visual Studio Code logo"
+              className={styles["skills-logo-vscode"]}
+            />
           </a>
           <a
             href="https://www.jetbrains.com/webstorm/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={webstormLogo} alt="webstorm logo" />
+            <img
+              src={webstormLogo}
+              alt="WebStorm logo"
+              className={styles["skills-logo-webstorm"]}
+            />
           </a>
         </div>
       </div>
