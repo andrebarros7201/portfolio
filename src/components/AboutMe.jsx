9b9fca91e5c3656c26071d5404f969378d88bfb7
Andre Barros <andrebarros7201@gmail.com>
2024-08-18 11:16:59 +0100

    update about me right section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,4 +1,6 @@
 import styles from "../styles/aboutMe.module.css";
+import linkedinLogo from "../assets/linkedin.svg";
+import githubLogo from "../assets/github.svg";
 
 export default function AboutMe() {
   return (
@@ -10,15 +12,45 @@ export default function AboutMe() {
         <p>Placeholder for an image</p>
       </div>
       <div className={styles["about-me-right"]}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste
-          laborum, a ratione dolor, omnis consequatur rem voluptatum officiis
-          vel itaque perferendis, repudiandae totam libero quaerat dicta dolores
-          optio earum quisquam. Dolor illum est et magnam, minima facere
-          accusantium non laudantium temporibus saepe. Voluptatem cum sunt nisi
-          aliquid, aperiam saepe hic perferendis ex soluta ipsa cumque
-          voluptates ut commodi expedita minima?
-        </p>
+        <div className={styles["about-me-right-text"]}>
+          <p>
+            I am a Front-End Developer passionate about creating engaging and
+            functional digital experiences. With a strong foundation in HTML,
+            CSS, JavaScript, and React, I have hands-on experience gained
+            through internships and personal projects, where I developed
+            responsive web applications focused on usability and user
+            experience.
+          </p>
+          <p>
+            I am always looking for new opportunities to apply my skills in
+            challenging projects, where I can contribute to creating digital
+            products that positively impact users&apos; lives.
+          </p>
+        </div>
+        <div className={styles["about-me-right-links"]}>
+          <a
+            href="https://github.com/andrebarros7201"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={githubLogo}
+              alt="github logo"
+              className={styles["skills-logo-github"]}
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/andr%C3%A9-barros-8698a1185/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={linkedinLogo}
+              alt="linkedin logo"
+              className={styles["skills-logo-linkedin"]}
+            />
+          </a>
+        </div>
       </div>
     </section>
   );
